aba41f108bba98d7e5a32770517f31b4cd6b9fb2
Apoc <47511820+tony27786@users.noreply.github.com>
2025-04-25 14:02:02 +0800

    Create sample_data.R

---
+++ sample_data.R
@@ -0,0 +1,18 @@
+load("~/working/test_dataset.RData")
+markers_df_common <- common_de_marker_selection(
+  de_list_1 = markers_df_hhc,
+  de_list_2 = markers_df_hth,
+  suffix_list = c("_hhc", "_hth"),
+  xtitle = "LogFC (HIV vs HC)",
+  ytitle = "LogFC (HIV_MTB vs HIV)"
+)
+mart <- biomaRt::useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+converted_common_markers <- common_de_geneid_converter(
+  common_de_list = markers_df_common$common_markers, mart = mart
+)
+common_enrich <- common_markers_enrichment(converted_common_markers)
+p1 <- common_plot_kegg_results(
+  common_enrich,
+  hjust = 1.7,
+  title = "KEGG for CD4 Naive in four quadrants"
+)
