64815180568cb8db6080a599983c0440a1434fb7
Apoc <47511820+tony27786@users.noreply.github.com>
2025-04-27 13:20:23 +0800

    Update sample_data.R

--- sample_data.R
+++ sample_data.R
@@ -9,7 +9,8 @@ markers_df_common <- common_de_marker_selection(
   de_list_2 = markers_df_hth,
   suffix_list = c("_hhc", "_hth"),
   xtitle = "LogFC (HIV vs HC)",
-  ytitle = "LogFC (HIV_MTB vs HIV)"
+  ytitle = "LogFC (HIV_MTB vs HIV)",
+  plot_title = "Common Gene LogFC Distribution among Three Groups"
 )
 mart <- biomaRt::useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 converted_common_markers <- common_de_geneid_converter(
